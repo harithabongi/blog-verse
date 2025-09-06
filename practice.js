d8cd30df5f0809707ed1182dd34533647b750ef1
harithabongi <harithabongi2007@gmail.com>
2025-09-06 12:34:28 +0530

    learn datatypes

--- practice.js
+++ practice.js
@@ -1,1 +1,22 @@
 console.log("hello world,haritha")
+let name="haritha";
+console.log(name)
+let num=11;
+console.log(num)
+console.log(typeof name)
+console.log(typeof num)
+let isLogged=false;
+console.log(typeof isLogged)
+let x;
+console.log(typeof x)
+let y=null;
+console.log(typeof y)
+let a=[1,2,3,4,5]
+console.log(a[1])
+console.log(typeof a)
+let person={
+    name:"haritha",
+    age:18
+}
+console.log(person.name)
+console.log(typeof person)
