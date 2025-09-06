cbde4a07948a316d4f03f8d71089ea70e07d55d1
harithabongi <harithabongi2007@gmail.com>
2025-09-06 15:54:19 +0530

    function

---
+++ function.js
@@ -0,0 +1,22 @@
+function greet()
+{
+    console.log("welcome")
+}
+greet()
+function add(x,y)
+{
+    return x+y
+}
+let result =add(2,3)
+console.log(result)
+const x=10
+const multiple=function(x,y)
+{
+    return x+y
+}
+let z =multiple(x,y=3)
+console.log(z)
+const square=(x)=>x*x
+console.log(square(11))
+const sub=(x,y)=>x-y
+console.log(sub(5,2))
